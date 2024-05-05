bc054314c2721f3d43985607d47dfd41b5f6bf8c
harshkchoudhary11902012 <choudharyharshofficial@gmail.com>
2024-05-05 22:24:30 +0530

    Working with multiple useStates in one form

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
-import Data from "./Data";
+// import Data from "./Data";
+import Form from "./Form";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Data />
+    {/* <App/> */}
+    {/* <Data /> */}
+    <Form />
   </React.StrictMode>
 );
